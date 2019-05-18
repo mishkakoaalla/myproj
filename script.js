ac4a3bf2aed8a228d50d0e42c8739ca4b4686c9f
mishka <mix007m7@gmal.com>
2019-05-18 17:34:58 +0300

    3 commit

--- script.js
+++ script.js
@@ -9,11 +9,7 @@ let hero = {
 let mass = ['balks','wheel','544','100110'];
 
 
-//alert("piiiiiiid");
-//let vopros = confirm("Ты робот");
-//console.log(vopros);
-//let vopros = prompt("Введи свой Ник", "urodec");
-//console.log(typeof(vopros));
+
 
 var prov = false;
 var prov_2 = true;

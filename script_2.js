c67b51a266ea297e6e0f987e6574610e89af3f40
mishka <mix007m7@gmal.com>
2019-05-18 17:37:48 +0300

    3 com _3
    Signed-off-by: mishka <mix007m7@gmal.com>

---
+++ script_2.js
@@ -0,0 +1,20 @@
+'use strict';
+let hero = {
+    name: "Ban",
+    mana: 100,
+    atack: true
+}
+
+
+let mass = ['balks','wheel','544','100110'];
+
+
+
+
+var prov = false;
+var prov_2 = true;
+
+console.log(prov || !prov_2);
+
+
+

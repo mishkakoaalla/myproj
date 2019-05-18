c98f3a3aa47839283abc8a0023703cedd2bba172
mishka <mix007m7@gmal.com>
2019-05-18 17:18:35 +0300

    1 -комит

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+let hero = {
+    name: "Ban",
+    mana: 100,
+    atack: true
+}
+
+
+let mass = ['balks','wheel','544','100110'];
+
+
+//alert("piiiiiiid");
+//let vopros = confirm("Ты робот");
+//console.log(vopros);
+//let vopros = prompt("Введи свой Ник", "urodec");
+//console.log(typeof(vopros));
+
+var prov = false;
+var prov_2 = true;
+
+console.log(prov || !prov_2);
+
+
+
